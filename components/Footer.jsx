814abaddcd49dfb6ced5de1d89caee95bb6da9f0
hardcoregor <spraytown2014@gmail.com>
2023-03-25 23:37:49 +0200

    text correct

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -25,28 +25,28 @@ const Footer = () => {
           <div className={`justify-center items-center text-white bg-block1 bg-no-repeat bg-cover bg-[center_top_-10px] xs:w-[100%] s:w-[90%] sm:h-1/2 sm:w-[47%] lg:h-[620px] lg:w-[47%] xl:h-[80%] xl:w-[34%] 2xl:h-[425px] 2xl:w-[360px] cursor-pointer`}>
             <div className='flex flex-col mt-[190px] items-center'>
               <h1 className='text-3xl sm:-mt-10 s:mt-20 md:mt-4 lg:mt-[120px] xl:text-4xl xl:mt-[120px] lg:text-5xl 2xl:mt-[8px] glowBtn'>Narrative</h1>
-              <p className={`xs:mb-14 sm:my-[20px] sm:pb-7 md:pb-20 lg:text-xl lg:w-[80%] lg:my-[20px] xl:w-full xl:text-sm font-thin text-[15px] leading-[17px] text-center px-10 ${raleway.className}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <p className={`xs:mb-14 sm:my-[20px] sm:pb-7 md:pb-20 lg:text-xl lg:w-[80%] lg:my-[20px] xl:w-full xl:text-sm font-thin text-[15px] leading-[17px] text-center px-10 ${raleway.className}`}>Storytelling through the creation of interactive games, comics with a compelling storyline and animated series.  </p>
             </div>
           </div>
 
           <div className={`justify-center items-center text-white bg-block2 bg-no-repeat bg-cover bg-[center_top_-10px] xs:w-[100%] s:w-[90%] sm:h-1/2 sm:w-[47%] lg:h-[620px] lg:w-[47%] xl:h-[80%] xl:w-[34%] 2xl:h-[425px] 2xl:w-[360px] cursor-pointer`}>
             <div className='flex flex-col mt-[190px] items-center'>
               <h1 className='text-3xl sm:-mt-10 s:mt-20 md:mt-4 lg:mt-[120px] xl:text-4xl xl:mt-[120px] lg:text-5xl 2xl:mt-[8px] glowBtn'>Innovation</h1>
-              <p className={`xs:mb-14 sm:my-[20px] sm:pb-7 md:pb-20 lg:text-xl lg:w-[80%] lg:my-[20px] xl:w-full xl:text-sm font-thin text-[15px] leading-[17px] text-center px-10 ${raleway.className}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <p className={`xs:mb-14 sm:my-[20px] sm:pb-7 md:pb-20 lg:text-xl lg:w-[80%] lg:my-[20px] xl:w-full xl:text-sm font-thin text-[15px] leading-[17px] text-center px-10 ${raleway.className}`}>Innovation, where the project's community influences the story and the development of the universe.</p>
             </div>
           </div>
 
           <div className={`justify-center items-center text-white bg-block3 bg-no-repeat bg-cover bg-[center_top_-10px] sm:mt-10 xs:w-[100%] s:w-[90%] sm:h-1/2 sm:w-[47%] lg:h-[620px] lg:w-[47%] xl:h-[80%] xl:w-[34%] 2xl:h-[425px] 2xl:w-[360px] xl:mt-0 cursor-pointer`}>
             <div className='flex flex-col mt-[190px] items-center'>
               <h1 className='text-3xl sm:-mt-10 s:mt-20 md:mt-4 lg:mt-[120px] xl:text-4xl xl:mt-[120px] lg:text-5xl 2xl:mt-[8px] glowBtn'>Worldbuilding</h1>
-              <p className={`xs:mb-14 sm:my-[20px] sm:pb-7 md:pb-20 lg:text-xl lg:w-[80%] lg:my-[20px] xl:w-full xl:text-sm font-thin text-[15px] leading-[17px] text-center px-10 ${raleway.className}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <p className={`xs:mb-14 sm:my-[20px] sm:pb-7 md:pb-20 lg:text-xl lg:w-[80%] lg:my-[20px] xl:w-full xl:text-sm font-thin text-[15px] leading-[17px] text-center px-10 ${raleway.className}`}>The unique style of the project and the constant development of the ecosystem.</p>
             </div>
           </div>
 
           <div className={`justify-center items-center text-white bg-block4 bg-no-repeat bg-cover bg-[center_top_-10px] sm:mt-10 xs:w-[100%] s:w-[90%] sm:h-1/2 sm:w-[47%] lg:h-[620px] lg:w-[47%] xl:h-[80%] xl:w-[34%] 2xl:h-[425px] 2xl:w-[360px] xl:mt-0 cursor-pointer`}>
             <div className='flex flex-col mt-[190px] items-center'>
               <h1 className='text-3xl sm:-mt-10 s:mt-20 md:mt-4 lg:mt-[120px] xl:text-4xl xl:mt-[120px] lg:text-5xl 2xl:mt-[8px] glowBtn'>Suns Studio</h1>
-              <p className={`xs:mb-14 sm:my-[20px] sm:pb-7 md:pb-20 lg:text-xl lg:w-[80%] lg:my-[20px] xl:w-full xl:text-sm font-thin text-[15px] leading-[17px] text-center px-10 ${raleway.className}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <p className={`xs:mb-14 sm:my-[20px] sm:pb-7 md:pb-20 lg:text-xl lg:w-[80%] lg:my-[20px] xl:w-full xl:text-sm font-thin text-[15px] leading-[17px] text-center px-10 ${raleway.className}`}>Integration of AI to scale the universe.</p>
             </div>
           </div>
         </div>
